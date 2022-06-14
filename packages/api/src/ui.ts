a3522106a6f2550ec3c9f84d0527db518339d088
Joshua Hansen <iamjoshhansen@gmail.com>
2022-06-14 16:17:30 -0600

    Fixed new routing issues

--- packages/api/src/ui.ts
+++ packages/api/src/ui.ts
@@ -6,14 +6,9 @@ export function initUi(app: Express) {
   app.use(bodyParser.json());
 
   const dist = path.resolve(__dirname + '/../../ui/dist/ui/');
-  // console.log({ dist });
-  app.use(express.static(dist));
+  app.use('/app/', express.static(dist, { redirect: false }));
 
-  app.get(['/app', '/app/*'], (req: Request, res: Response) => {
-    const file = req.path.substring(4) || 'index.html';
-
-    const finalPath = path.join(dist, file);
-    console.log({ dist, file, finalPath });
-    res.sendFile(finalPath);
+  app.get('*', function (req, res, next) {
+    res.sendFile(path.resolve(path.join(dist, 'index.html')));
   });
 }
